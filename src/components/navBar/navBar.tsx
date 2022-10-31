7d02650ab634d8684de0a8b4e9926f9879b10346
DeepthiNayak20 <deepthi.nayak@robosoftin.com>
2022-10-31 17:16:16 +0530

    first commit

---
+++ src/components/navBar/navBar.tsx
@@ -0,0 +1,23 @@
+import "./navBar.css";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div className="navBar">
+        <div className="logo">
+          <img src={require("../../assets/images/logo.png")} alt="logo" />
+        </div>
+        <div className="inputSearch">
+          <img
+            src={require("../../assets/icons/Shape.png")}
+            alt=""
+            className="searchIcon"
+          />
+          <input type="text" className="searchBar" placeholder="Search" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/views/homePage/homePage.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../../components/navBar/navBar";
+import "./homePage.css";
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default HomePage;
